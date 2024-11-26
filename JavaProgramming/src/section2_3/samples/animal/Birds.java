33266f813a00ec89493285f7c5f727be1821fad3
Fagner Romão <fagner.romao@gmail.com>
2024-11-25 23:56:06 -0300

    JavaProgramming - Section2

---
+++ JavaProgramming/src/section2_3/samples/animal/Birds.java
@@ -0,0 +1,5 @@
+package section2_3.samples.animal;
+
+public abstract class Birds extends Chordate implements Aerial,Terrestrial {
+
+}
---
+++ JavaProgramming/src/section2_3/samples/animal/Chordate.java
@@ -0,0 +1,13 @@
+package section2_3.samples.animal;
+
+public abstract class Chordate extends Animal{
+	
+	public Chordate()
+	{}	
+	
+	public Chordate(String scientificName,String popularName,int age,double mass) {
+		super(scientificName,popularName,age,mass);
+		
+	}
+	
+}
