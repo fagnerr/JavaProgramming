00f3a9961876a7a495de0d4aa730a9fc41b5b96e
Fagner Romão <fagner.romao@gmail.com>
2024-11-25 23:59:21 -0300

    JavaProgramming - Section2

---
+++ JavaProgramming/src/section2_3/main/AbstractClass.java
@@ -0,0 +1,27 @@
+package section2_3.main;
+
+
+public abstract class AbstractClass implements Interface1,Interface2 {
+	
+	//Properties
+	public long password;
+	
+	public AbstractClass()
+	{
+		password=9999;
+	}
+	
+	public AbstractClass(long password)
+	{
+		this.password=password;
+	}
+	
+	//Behaviors
+	public void sayHello() {
+		System.out.println("Olá!");
+	}
+	
+	
+	public abstract void sayHi(); 
+	
+}
