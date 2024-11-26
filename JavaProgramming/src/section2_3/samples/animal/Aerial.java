2e4705802083a8c7db4648badd8e9ff95c0f139b
Fagner Romão <fagner.romao@gmail.com>
2024-11-25 23:55:42 -0300

    JavaProgramming - Section2

---
+++ JavaProgramming/src/section2_3/samples/animal/Aerial.java
@@ -0,0 +1,6 @@
+package section2_3.samples.animal;
+
+public interface Aerial {
+	
+	void fly();
+}
---
+++ JavaProgramming/src/section2_3/samples/animal/Amphibian.java
@@ -0,0 +1,5 @@
+package section2_3.samples.animal;
+
+public abstract class Amphibian extends Chordate implements Aquatic,Terrestrial {
+
+}
